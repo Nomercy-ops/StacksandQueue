0ab9cd5ac1a1cbc443569b6e2fb16f6d41f66fe7
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-14 09:49:07 +0530

    Refactor: previous code and added methods for peek push pop

--- StackOperations.java
+++ StackOperations.java
@@ -2,7 +2,7 @@
  *
  * @author Rikesh Chhetri
  * @version 1.0
- * @Created_on: 07.06.21
+ * @Created_on: 14.06.21
  *
  * purpose: is to do the stack operations using linked list.
  * 
@@ -11,88 +11,105 @@
 
 package com.bridgelabz.datastructure.StackandQueue;
 
-public class StackOperations {
+public class StackOperations<T extends Comparable<T>>{
+Node<T> head ;
 
-    MyNode top;
-    // Constructor
-
-    StackOperations() {
-        this.top = null;
-    }
-
-    /**
-     *  Method for inserting elements into the stack.
-     * This method will push the element into the stack.
-     * @param x 
-     */
-    
-    public void push(int x) 
-    {
-        
-        MyNode temp = new MyNode();
-        if (temp == null) {
-            System.out.print("\nHeap Overflow");
-            return;
-        }
-        temp.data = x;
-        temp.link = top;
-        top = temp;
-    }
-
-   /**
-    * Method for checking if an stack is empty or not and return true or false.
-    * @return 
-    */
-    
-    public boolean isEmpty() {
-        return top == null;
-    }
-    
 /**
- * This method is used to return top element from the stack. 
- * @return 
+ * creates a empty stacks
  */
-    
-    public int peek() {
-        // check for empty stack
-        if (!isEmpty()) {
-            return top.data;
-        } else {
-            System.out.println("Stack is empty");
-            return -1;
-        }
-    }
-
-  /**
-   *  This method is used to pop out the top of the element from the stack.
-   */
-    
-    public void pop() 
-    {
-       // check for stack underflow
-        if (top == null) {
-            System.out.print("\nStack Underflow");
-            return;
-        }
-        top = (top).link;
-    }
-    
-    /**
-     * 
-     */
-
-    public void display() {
-        // check for stack underflow
-        if (top == null) {
-            System.out.printf("\nStack Underflow");
-            
-        } else {
-            MyNode temp = top;
-            while (temp != null) {
-                System.out.printf("%d->", temp.data);
-                temp = temp.link;
-            }
-        }
-    }
+ 
+public  Stack() {
+	head = null;
 }
 
+/**
+ *@purpose adds a new item to the top of the stack
+ * @param item it needs a item
+ * @return it returns nothing 
+ */
+public void push(T item) { 
+	Node<T> node =new Node<T>(item);
+	node.next =head;
+	head = node;
+}
+/**
+ * @purpose removes the top item from the stack 
+ * @param  it needs nothing 
+ * @return returns the top item
+ */
+
+public T pop() {
+	T data = null;
+	if(head == null) {
+		System.out.println("stack is empty");
+		return null;
+	}
+	else {
+		data = (T) head.data;
+		head = head.next;
+	}
+	return data;
+}
+	
+/**
+ * Returns top item from stack but does not remove it.
+ * @param it needs nothing 
+ * @return it return top item from stack
+ */
+	
+
+public T peek() {
+	T data =  null;
+	if( head  ==  null) {
+		System.out.println(" stack is empty");
+		return null;
+	}
+	else {
+		data = (T) head.data;
+	}
+	return data;
+}
+
+/**
+ * To see wheather the stack is empty or not
+ * @param it needs nothing 
+ * @return it returns boolean value 
+ */
+public boolean isEmpty() {
+return head==null?true:false;
+}
+
+/**
+ * checks the size of the stack 
+ * @param  it needs nothing
+ * @return it returns INTEGER to  show size
+ */
+public int size() {
+	Node<T> temp = head;
+	int count= 0 ;
+	while(temp.next!=null) {
+		count++;
+		temp = temp.next;  
+	}
+	return count; 
+}
+
+/**
+ * To display the stack
+ * @param   :it needs nothing
+ * @return  :returns nothing (void)
+ */
+
+public void display() {
+	Node<T> temp = head;
+	int count = 0  ; 
+	while(temp!=null) {
+		if(count%2 == 0 ) {
+			System.out.println();
+		}
+		System.out.print(temp.data+" ");
+		temp = temp.next;
+		count++;
+	}
+}
+}
